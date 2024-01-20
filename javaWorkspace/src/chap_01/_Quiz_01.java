cd916fdf2580261e2342de3aa22f8b6c019c01fc
Kyul <developer.kyul@gmail.com>
2024-01-21 00:39:00 +0900

    [study] chap_01
    환경설정>
    윈도우-JDK(adoptium) 설치: (6:18)
    윈도우-IDE(intelliJ) 설치: (15:52)
    Mac-JDK(Homebrew) 설치: (22:16)
    
    <Chapter01>
    01. Hello World: (36:32)
    02. 자료형: (45:46)
    03. 변수(전반전) (52:29)
    04. 변수(후반전) (1:03:37)
    05. 주석 (1:11:32)
    06. 변수 이름 짓는 법 (1:20:43)
    07. 상수 (1:33:14)
    08. 형변환(전반전) (1:37:11)
           형변환(후반전) (1:47:44)
           <퀴즈 #1> (1:54:31)

---
+++ javaWorkspace/src/chap_01/_Quiz_01.java
@@ -0,0 +1,13 @@
+package chap_01;
+
+public class _Quiz_01 {
+    public static void main(String[] args) {
+        String busNum = "상암08번";
+        int min = 3;
+        double limit = 1.2;
+        
+        System.out.println( busNum+ " 버스");
+        System.out.println("약 " + min + "분 후 도착" );
+        System.out.println("남은 거리 " + limit + "Km" );
+    }
+}
